7accaf5ad460d12f091eba3fb67798d177e1793c
Zachary Churchill <zacharyachurchill@gmail.com>
2019-11-26 20:45:31 -0500

    test

--- Main.hs
+++ Main.hs
@@ -14,6 +14,8 @@ import Network.TLS
 import Network.Connection (TLSSettings(..))
 import Data.Default.Class
 import Data.ByteString (ByteString)
+import Network.Connection
+import qualified Data.ByteString as B
 
 data Opts = Opts
   { ldapHost :: String
@@ -25,11 +27,30 @@ instance ParseRecord Opts
 
 main :: IO ()
 main = do
-  Opts{..} <- getRecord "Test program"
+  opts <- getRecord "Test program"
   tlsSettings <- either error id <$> getTlsSettings
+  testConnection opts tlsSettings
+  testLdapClient opts tlsSettings
+  
+testLdapClient :: Opts -> TLSSettings -> IO ()
+testLdapClient Opts{..} tlsSettings = do
   errorOrSuccess <- Ldap.with (Tls ldapHost tlsSettings) 636 $ \l -> do
     bindEither l (Dn ldapUpn) (Password ldapPassword)
   print errorOrSuccess
+
+testConnection :: Opts -> TLSSettings -> IO ()
+testConnection Opts{..} tlsSettings = do
+  ctx <- initConnectionContext
+  con <- connectTo ctx $ ConnectionParams
+    { connectionHostname  = ldapHost
+    , connectionPort      = 636
+    , connectionUseSecure = Just tlsSettings
+    , connectionUseSocks  = Nothing
+    }
+  connectionPut con (B.singleton 0xa)
+  r <- connectionGet con 1024
+  putStrLn $ show r
+  connectionClose con
 
 getTlsSettings :: IO (Either String TLSSettings)
 getTlsSettings = do
